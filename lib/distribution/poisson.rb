773e45afed76602e4b4b46fe843420674f6e79d0
Victor Pereira <vpereira@web.de>
2013-02-28 14:28:51 +0100

    extended poisson distribution with sigma, kurtosis and skewness methods

--- lib/distribution/poisson.rb
+++ lib/distribution/poisson.rb
@@ -1,7 +1,8 @@
 module Distribution
 	class Poisson < BaseDistribution
 		attr_accessor :k
-		
+		attr_reader :mean
+
 		def initialize(opts = {})
 			@mean = opts[:mean]
 			@k =  Vector.alloc( opts[:k] || [0] )
@@ -19,5 +20,18 @@ module Distribution
 			end
 			@k.collect { |k| Cdf.send(method_to_call,k, @mean.to_f) }
 		end
+
+		def sigma
+			sqrt(@mean)
+		end
+
+		def kurtosis
+			pow_int(@mean,-1)
+		end
+
+		def skewness
+			pow_int(@mean,0.5)
+		end
+
 	end
 end
