0eaad4dbaf71b25ec515fdb4b624801f82258c6e
Victor Pereira <vpereira@web.de>
2013-02-28 09:46:46 +0100

    added standard error and t value to Sample

--- lib/distribution/sample.rb
+++ lib/distribution/sample.rb
@@ -1,4 +1,7 @@
 module Distribution
+
+	class PopulationUnkownException < Exception;end
+
 	class Sample
 		include GSL
 
@@ -8,6 +11,16 @@ module Distribution
 			@type = opts[:type] || :discrete # :discrete | :continuous 
 			@distribution = opts[:distribution] # [:normal,:binomial,:poisson,:t]
 			@cases = @type == :discrete ? GSL::Vector.alloc(opts[:cases].round.to_a) : opts[:cases]
+			@population_n = opts[:population] || :unknown
+		end
+
+		def t
+			raise PopulationUnkownException,"You didn't provide the population size" if @population_n == :unknown
+			(@population_n - n) / sampling_error
+		end
+
+		def sampling_error
+			sigma / sqrt(n)
 		end
 
 		def mean
