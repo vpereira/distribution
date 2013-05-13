aeb7e948d2e35ab1ac38a1b281128b67b7e86526
Victor Pereira <vpereira@web.de>
2013-05-13 21:26:19 +0200

    pushed jruby version from exponential

---
+++ lib/distribution/jruby/exponential.rb
@@ -0,0 +1,29 @@
+require 'java'
+require_relative '../common_maths/commons-math3-3.2.jar'
+java_import 'org.apache.commons.math3.distribution.ExponentialDistribution'
+
+module Distribution
+	class Exponential
+		attr_accessor :mean,:k,:precision
+
+		def initialize(opts = {})
+			@mean = opts[:mean] || 1
+			@k =  opts[:k] || [0] 
+			@precision = opts[:precision] || 3
+			@d_handle = ExponentialDistribution.new(@mean) 
+		end
+		
+		def pdf
+			@k.collect { |k| @d_handle.density(k).round(@precision) }
+		end
+		
+		def cdf(lower_tail=true)
+			if lower_tail
+				@k.collect { |k|      @d_handle.cumulative_probability(k).round(@precision) }
+			else
+				@k.collect { |k|  1 - @d_handle.cumulative_probability(k).round(@precision)	}
+			end
+		end
+
+	end
+end
