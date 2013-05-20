e512f87b8a2547dfc56c898fc41423044a3523cd
Victor Pereira <vpereira@web.de>
2013-05-20 21:45:23 +0200

    plot is working. but didn't convince me

--- lib/distribution/jruby/distribution.rb
+++ lib/distribution/jruby/distribution.rb
@@ -1,7 +1,11 @@
 require 'java'
+
 require_relative '../common_maths/commons-math3-3.2.jar'
+
 java_import 'org.apache.commons.math3.analysis.function.Sqrt'
 java_import 'org.apache.commons.math3.random.EmpiricalDistribution'
+java_import 'org.apache.commons.math3.stat.descriptive.SummaryStatistics'
+
 module Distribution
 	module Utils
 		def sqrt(v)
@@ -17,18 +21,35 @@ module Distribution
 		#as parameter you should pass an array or GSL::Vector
 		def initialize(opts = {})
 			@data = opts[:data] || []
-			@hist = EmpiricalDistribution.new  
+			@num_bins = opts[:nbins] || 20
+			@hist = EmpiricalDistribution.new(@num_bins)  
 		end
 		def gen
 			#temporary
 			@hist.load(data.to_java(Java::double))
 			@hist
 		end
-		def graph
-			raise NotImplemented
+		def plot
+			raise HistogramUnkownException,"You didn't call gen before" if @hist.nil?
+			IO.popen("graph -T X -g 3", "r+") do |graph|
+				gen_histogram.each_with_index do |l,i|
+					graph.printf("%e %e\n",i+=1,l)
+				end
+				graph.close_write
+			end
 		end
 		def mean
 			@hist.get_numerical_mean
+		end
+
+		private
+
+		def gen_histogram
+			histogram = []
+			@hist.get_bin_stats.each_with_index do |stat,i|
+				histogram[i] = stat.get_n
+			end
+			histogram
 		end
 	end
 end
